6d97a4d9cab3a0444bdaef5fd6af9d11018b021e
RyanSchreiber <ryanschreiber93@gmail.com>
2015-01-10 14:56:41 -0800

    Map updated
    added firebase

--- GoogleMap/Map.js
+++ GoogleMap/Map.js
@@ -1,89 +1,43 @@
-var SF = new google.maps.LatLng(36.991429, -122.060817)
+var ref = new Firebase("https://leaving-now.firebaseio.com/Users/482331016");
+var map;
+var marker;
 
-function calcRoute() {
-	var directionsService = new google.maps.DirectionsService();
-	var directionsDisplay = new google.maps.DirectionsRenderer();
-	directionsService.route(request, function(response, status) {
-		if (status == google.maps.DirectionsStatus.OK) {
-			directionsDisplay.setDirections(response);
-		}
-	});
+function initialize() {
+  var myLatlng = new google.maps.LatLng(36.9741667,-122.0297222
+
+);
+  var mapOptions = {
+    zoom: 7,
+    center: myLatlng
+  }
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'Hello World!'
+  });
+  moveMarker(map, marker);
 }
 
-function initialize(){
-	var	directionsDisplay = new google.maps.DirectionsRenderer();
-	var mapProp = {
-		center: SF,
-		zoom:7,
-		panControl:true,
-		zoomControl:true,
-		mapTypeControl:true,
-		scaleControl:true,
-		streetViewControl:true,
-		overviewMapControl:true,
-		rotateControl:true,    
-		mapTypeId: google.maps.MapTypeId.HYBRID
-	};
- 	var contentString = '<div id="content">'+
-		'<div id="siteNotice">'+
-		'</div>'+
-		'<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-		'<div id="bodyContent">'+
-		'<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-		'sandstone rock formation in the southern part of the '+
-		'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-		'south west of the nearest large town, Alice Springs; 450&#160;km '+
-		'(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-		'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-		'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-		'Aboriginal people of the area. It has many springs, waterholes, '+
-		'rock caves and ancient paintings. Uluru is listed as a World '+
-		'Heritage Site.</p>'+
-		'<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-		'http://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-		'(last visited June 22, 2009).</p>'+
-		'</div>'+
-		'</div>'; 
-
-	var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-	calcRoute()
-	function isInfoWindowOpen(infoWindow){
-		var map = infoWindow.getMap();
-		return (map === null || typeof map === "undefined");
-	}
-	
-	var marker = new google.maps.Marker({
-		position:SF,
-	});
-
-	//marker.setMap(map);
-	directionsDisplay.setMap(map);
-
-	// Zoom to 20 when clicking on marker
-	google.maps.event.addListener(marker,'dblclick',function() {
-		map.setZoom(20);
-		map.setCenter(marker.getPosition());
-	});
-	
-	google.maps.event.addListener(marker,'click',function() {
-		infowindow2.open(map, marker);
-		if (isInfoWindowOpen(infowindow2)){
-			marker.setAnimation(google.maps.Animation.BOUNCE);
-		}
-	});
-
-	
-	marker.setMap(map);
-	
-	google.maps.event.addListener(marker,'click',function() {
-		infowindow.open(map, marker);
-
-	});
-
-	var sanFranControl = new SFControl(homeControlDiv, map);
-
-}
 google.maps.event.addDomListener(window, 'load', initialize);
 
+
+
+function moveMarker( map, marker ) {
+    
+    //delayed so you can see it move
+    setTimeout( function(){
+        marker.setPosition( new google.maps.LatLng( 0, 0 ) );
+        map.panTo( new google.maps.LatLng( 0, 0 ) );
+        
+    }, 1500 );
+
+};
+
+ref.on("child_changed", function(snapshot) {
+  var changedPost = snapshot.val();
+  console.log("The updated post long is " + changedPost.Long);
+});
 
 
